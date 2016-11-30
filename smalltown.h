c3ec6071a605b4274b59ea6f32b8de6b26a15af6
Sebastian Kot <sk371043@students.mimuw.edu.pl>
2016-11-30 12:35:23 +0100

    small updates to smalltown

--- smalltown.h
+++ smalltown.h
@@ -6,34 +6,78 @@
 #include <cstddef>
 #include "citizen.h"
 #include "monster.h"
+#include <type_traits>
+#include <typeinfo>
+#include <iostream>
+#include <tuple>
 
 namespace {
-	//TODO: constexpr na liczby Fibonacciego
-};
+    constexpr unsigned giveDesiredFibbonacci(unsigned index) {
+        return index <= 1 ? 1 : giveDesiredFibbonacci(index - 2) +
+            giveDesiredFibbonacci(index - 1);
+    }
 
+    constexpr unsigned giveFibArraySize(unsigned maxNumber, unsigned index) {
+        return giveDesiredFibbonacci(index) > maxNumber ? index : giveFibArraySize(maxNumber, index + 1);
+    }
 
-// jakis static assert na t0 i t1
-template <typename M, typename U, U t0, U t1, typename... C>
+    template <unsigned...args>
+    constexpr std::array <unsigned, sizeof...(args)> giveArray(std::integer_sequence<unsigned,args...>) {
+        return std::array <unsigned, sizeof...(args)> {{giveDesiredFibbonacci(args)...}};
+    }
+}
+
+template <typename M, typename U, U t0, U t1, typename... C> //C same sie sprawdza czy dobre typy
 class SmallTown {
-private:
-	//TODO
-	// potwor
-	M _monster;
-	// Wektor zywych mieszkancow 
-	// pytanie czy w ten sposob nie stracimy informacji 
-	// o tym kto jest szeryfem przy przeladowaniu attack()...
-	vector<Citizen> _citizens; 
-public:
-	SmallTown(M monster, C... citizens) {
-
-	}
-
-	std::tuple<std::string, T, size_t> & getStatus() {
-		//std::string = m.valueType;
-		//T = m.getHealth()
-		//size_t = _citizens.size()
-	}
-	void tick(U timeStep);
+    private:
+        static constexpr unsigned _sizeOfArray = giveFibArraySize((unsigned)t1, 0);
+        static constexpr std::array<unsigned, _sizeOfArray> _fibbonacciArray = giveArray(std::make_integer_sequence<unsigned, _sizeOfArray>{});
+        U _currentTime;
+        U _cycleTimer;
+        M _monster;
+        std::tuple<C...> _citizens;
+        unsigned _alive;
+    public:
+        constexpr SmallTown(M monster, C... citizens) :
+           _currentTime(t0), _cycleTimer(t1), _monster(monster),
+           _citizens(citizens...), _alive(sizeof...(C)) {
+               static_assert(std::is_integral<U>::value, "Type not arythmetic");
+               static_assert((t0 >= 0) && (t1 >= 0) && (t0 < t1), "Timestamps invalid");
+        }
+            
+        auto getStatus() {
+            return std::make_tuple(_monster.valueType, _monster.getHealth(), _alive);
+        }
+        
+        void TESTOWEprntfib() { //TODO: do usuniecia
+            for(auto &in : _fibbonacciArray) {
+                std::cout << in << " ";
+            }
+        }
+        
+        void tick(U timeStep) {
+            bool annihilationTime = false;
+            U steppedTime = (_currentTime + timeStep)%_cycleTimer;
+            
+            for (auto &in : _fibbonacciArray) {
+                if (in == steppedTime) annihilationTime = true; 
+            }
+            
+            if (annihilationTime) 
+                attackAll();
+        };
+        
+        private:
+            void attackAll() {
+                
+            }
 };
+
+
+template <typename M, typename U, U t0, U t1, typename...C>
+constexpr unsigned SmallTown<M, U, t0, t1, C...>::_sizeOfArray;
+
+template <typename M, typename U, U t0, U t1, typename...C>
+constexpr std::array<unsigned, SmallTown<M, U, t0, t1, C...>::_sizeOfArray> SmallTown<M, U, t0, t1, C...>::_fibbonacciArray;
 
 #endif /* __SMALLTOWN_H__ */
