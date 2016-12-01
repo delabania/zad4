81e2a87df53775dd14000a3e4f7174d25055855d
delabania <przem0.b95@gmail.com>
2016-12-01 21:33:26 +0100

    test2

--- smalltown_test.cc
+++ smalltown_test.cc
@@ -9,7 +9,19 @@
 
 using namespace std;
 
+void test1();
+void test2();
+
 int main() {
+
+	//test1();
+	test2();
+	return 0;
+}
+
+
+
+void test1() {
 	auto smallTown = SmallTown < Mummy<float>, int, 3, 40,
 	     Sheriff<int>, Adult<int>, Teenager<long>, Adult<float> >
 	     (Mummy<float>(100.0f, 25.0f),
@@ -61,11 +73,79 @@ int main() {
 	assert(get<1>(status) == 20);
 	assert(get<2>(status) == 0);
 	//(20, 0, 0, 0, 0)
+}
 
+//test na to czy tick dobrze dziala przy przeskakiwaniu powyzej t1
+void test2() {
+	auto smallTown = SmallTown<Vampire<int>, int, 6, 10, Adult<int> > (
+	                     Vampire<int>(100, 1), Adult<int>(10, 18)
+	                 );
 
+	smallTown.tick(4); // 6->10
+	//(100, 10)
+	smallTown.tick(1); // 10->1
+	//(100, 10)
+	smallTown.tick(1); // 1->2
+	//(100, 9)
+	smallTown.tick(1); // 2->3
+	//(100, 8)
+	smallTown.tick(2); // 3->5
+	//(100, 7)
+	smallTown.tick(3); // 5->8
+	//(100, 6)
+	smallTown.tick(3); // 8->1
+	//(100, 5)
+	smallTown.tick(4); // 1->5
+	//(100, 4)
+	smallTown.tick(3); // 5->8
+	//(100, 3)
+	smallTown.tick(10); // 8->8
+	//(100, 2)
+	smallTown.tick(1); // 8->9
+	//(100, 1)
+	auto status = smallTown.getStatus();
+	assert(get<1>(status) == 100);
+	assert(get<2>(status) == 1);
 
+	smallTown.tick(2); // 9->1
+	//(100, 1)
+	smallTown.tick(5);
 	status = smallTown.getStatus();
+	assert(get<1>(status) == 100);
+	assert(get<2>(status) == 0);
+	//(100, 0)
+
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(3); // wypisanie MONSTER WON
+	smallTown.tick(111); // wypisanie MONSTER WON
+	smallTown.tick(19239239U); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+	smallTown.tick(1); // wypisanie MONSTER WON
+
+	smallTown.tick(1); // wypisanie MONSTER WON
+
+	// Powinien wypisac 12 MONSTER WON
 
 
-	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
